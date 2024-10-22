65f4911b0cf36aee66130ea74b5205307eb007eb
José Carlos Juárez <64013592+JJuarez-git@users.noreply.github.com>
2024-10-22 14:44:38 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React + Jenkins 1
+          Learn React + Jenkins 1 2
         </a>
       </header>
     </div>
